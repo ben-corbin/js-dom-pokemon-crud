83f6edfbede11676293d98c1e6d2fecd8839c50a
Ben Corbin <ben@corbin.digital>
2022-02-16 17:01:32 +0000

    All parts complete

--- src/index.js
+++ src/index.js
@@ -1,55 +1,89 @@
-const pokeForm = document.querySelector(".poke-form");
-const pokeList = document.querySelector(".poke-list");
+const pokeForm = document.querySelector(".poke-form")
+const pokeList = document.querySelector(".poke-list")
 
 function addPokemon(pokemon) {
-  const liEl = document.createElement("li");
-  const imgEl = document.createElement("img");
-  const h2El = document.createElement("h2");
+  const liEl = document.createElement("li")
+  const imgEl = document.createElement("img")
+  const h2El = document.createElement("h2")
+  const buttonEl = document.createElement("button")
+  const buttonElTwo = document.createElement("button")
 
-  liEl.classList.add("pokemon");
-  imgEl.src = pokemon.image;
+  buttonEl.addEventListener("click", function() {
+    fetch("http://localhost:3000/pokemons/" + pokemon.id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pokemon),
+    })
 
-  h2El.innerText = pokemon.name;
+})
 
-  liEl.append(imgEl, h2El);
-  pokeList.append(liEl);
+buttonElTwo.addEventListener("click", function() {
+  fetch("http://localhost:3000/pokemons/" + pokemon.id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({like: 'YEAH BOI'}),
+  })
+
+})
+
+buttonElTwo.addEventListener("click", function() {
+  console.log("click")
+})
+
+liEl.classList.add("pokemon")
+
+imgEl.src = pokemon.image
+
+h2El.innerText = pokemon.name
+
+buttonEl.innerText = ("REMOVE")
+buttonElTwo.innerText = ("LIKE")
+
+liEl.append(imgEl, h2El, buttonEl, buttonElTwo)
+pokeList.append(liEl)
 }
 
 function addPokemons(pokemons) {
-  pokemons.forEach(pokemon => addPokemon(pokemon))
+  console.log(pokemons)
+  pokemons.forEach((pokemon) => addPokemon(pokemon))
 }
 
 function listenToAddPokemonForm() {
-  pokeForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const pokemon = {
-      name: pokeForm.name.value,
-      image: pokeForm.image.value
-    };
+  pokeForm
+    .addEventListener("submit", function (event) {
+      event.preventDefault()
+      const pokemon = {
+        name: pokeForm.name.value,
+        image: pokeForm.image.value,
+      }
 
-    // CREATE
-    // fetch("http://localhost:3000/pokemons", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify(pokemon)
-    // })
-    //   .then(res =>  res.json())
-    //   .then(pokemon => addPokemon(pokemon));
-    //   });
+      // CREATE
+      fetch("http://localhost:3000/pokemons", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pokemon),
+      })
 
-    pokeForm.reset();
-  });
+      pokeForm.reset()
+      .then((res) => res.json())
+     .then((pokemon) => addPokemon(pokemon))
+    })
+    
 }
 
 function init() {
-  listenToAddPokemonForm();
+  listenToAddPokemonForm()
 
   // READ
-  // fetch("http://localhost:3000/pokemons")
-  //   .then(res => res.json());
-  //   .then(pokemons => addPokemons(pokemons));
+  fetch("http://localhost:3000/pokemons")
+    .then((res) => res.json())
+    .then((pokemons) => addPokemons(pokemons))
 }
 
-init();
+init()
